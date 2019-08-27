a9345fc4da61ee0217e84111ebe536b22bacba3b
Ryan Lewis (NWIS - Software Development) <Ryan.Lewis@wales.nhs.uk>
2019-08-27 16:33:41 +0100

    began refactoring to mvvm

---
+++ ContactsList/Repositories/ContactRepo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactsList.Repositories
+{
+    public class ContactRepo : IContactRepo
+    {
+        ContactDb _context = new ContactDb();
+    }
+}
